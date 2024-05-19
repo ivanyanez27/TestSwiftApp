0669c48bab5eb0dae9c0955ec4dd8f0eed91b8d0
ivanyanez27 <ivanyanez2712@gmail.com>
2024-05-19 17:05:58 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Ivan Yañez on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
